e8e67a31a4291d4f2307a02d44cb0fc8c0337732
Gabriel Walters <gwalters@lhschools.org>
2019-09-26 09:51:16 -0400

    added todo steps

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -3,6 +3,44 @@ package org.LickingHeights;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //todo declare variables a,b,c,discriminant, axisOfSymmetry,parabolaFacing, x, y
+
+        //todo initialize manually
+
+        //todo do calculations
+
+        //todo print results
+
+        //todo not in code check results
+
+        //todo initialize scanner and redo the initialize area
+
+        //todo double check results
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
